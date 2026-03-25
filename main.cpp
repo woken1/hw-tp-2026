30c94961369a7ccc1d87b50569931b1d78a3bead
woken1 <82443038+woken1@users.noreply.github.com>
2026-03-25 23:05:50 +0300

    Update main.cpp
    2

--- main.cpp
+++ main.cpp
@@ -20,6 +20,36 @@ public:
     length_error(const char* msg) : my_exception(msg) {}
 };
 
+template< class T >
+struct default_compare {
+    bool operator()(const T& a, const T& b) const {
+        return a < b;
+    }
+};
+
+template< class T, class Compare = default_compare<T> >
+T* merge(const T* a, size_t sa,
+         const T* b, size_t sb,
+         T* c, size_t sc,
+         Compare comp = Compare())
+{
+    if (!a && sa != 0) {
+        throw invalid_argument("merge: null pointer a with non-zero size");
+    }
+    if (!b && sb != 0) {
+        throw invalid_argument("merge: null pointer b with non-zero size");
+    }
+    if (!c) {
+        throw invalid_argument("merge: null output pointer c");
+    }
+    
+    if (sc < sa + sb) {
+        throw length_error("merge: output buffer too small");
+    }
+    
+    return c;
+}
+
 int main() {
     return 0;
 }
